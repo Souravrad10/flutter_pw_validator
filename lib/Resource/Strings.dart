e19a0e14026052272821166683f843eecf7b772c
Sourav Behera <sbehera@radiansys.com>
2022-05-25 16:36:57 +0530

    fix: change resource strings content

--- lib/Resource/Strings.dart
+++ lib/Resource/Strings.dart
@@ -1,6 +1,6 @@
 /// Strings hold constant strings used across the package
 class FlutterPwValidatorStrings {
-  final String minLength = "At least - character";
+  final String minLength = "At least - characters";
   final String capitalLetters = "At least - capital letter";
   final String smallLetters = "At least - small letter";
   final String number = "At least - number";
